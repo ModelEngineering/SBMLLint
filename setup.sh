40ffedea7a1b4d868afad9fdcc71251d93eab665
Joseph Hellerstein <jlheller@uw.edu>
2017-08-29 12:36:20 -0700

    Changed source for libsbml

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 # Note that you must have conda (or miniconda) installed to 
 # run this script.
 conda install -c sys-bio tellurium
-conda install -c SBMLTeam python-libsbml
+pip install python-libsbml
