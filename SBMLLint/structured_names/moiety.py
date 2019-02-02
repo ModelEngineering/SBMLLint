5e8ecef8b5435fedecb6e70b89fe51c1a520bd5d
Joe Hellerstein <jlheller@uw.edu>
2019-02-02 12:48:47 -0800

    Implemented MoietyComparator to compare the moieties of sets of molecules.

--- SBMLLint/structured_names/moiety.py
+++ SBMLLint/structured_names/moiety.py
@@ -53,4 +53,44 @@ class Moiety(object):
       moietys.extend([m.name for m in cls.extract(molecule)])
     df = pd.DataFrame({cn.VALUE: moietys})
     return pd.DataFrame(df.groupby(cn.VALUE).size())
-    
+
+
+class MoietyComparator(object):
+  """Analysis of moieties of molecules."""
+
+  def __init__(self, molecules1, molecules2):
+    """
+    :param set-Molecule molecules1:
+    :param set-Molecule molecules2:
+    """
+    self.list_of_molecules = [molecules1, molecules2]
+
+  def isSame(self):
+    """
+    Determines if the two molecules have the same type and count
+    of moieties.
+    :return bool:
+    """
+    df0 = Moiety.countMoietys(self.list_of_molecules[0])
+    df1 = Moiety.countMoietys(self.list_of_molecules[1])
+    return df0.equals(df1)
+
+  def difference(self):
+    """
+    Calculates the moieties present in one set and not in the other.
+    :return pd.DataFrame: row index is moiety,
+        column Value: counts of moieties present in 0 
+        and not in second. Negative values are present in 1 and
+        not in 0.
+    """
+    def addDFIndex(df, index):
+      # Adds missing indices to df
+      missing = set(index).difference(df.index)
+      for item in missing:
+        df.loc[item] = 0
+    #
+    df0 = Moiety.countMoietys(self.list_of_molecules[0])
+    df1 = Moiety.countMoietys(self.list_of_molecules[1])
+    addDFIndex(df0, df1.index)
+    addDFIndex(df1, df0.index)
+    return df0 - df1
--- SBMLLint/tests/structured_names/test_moiety.py
+++ SBMLLint/tests/structured_names/test_moiety.py
@@ -5,8 +5,9 @@ from SBMLLint.common import constants as cn
 from SBMLLint.common.molecule import Molecule
 from SBMLLint.common.simple_sbml import SimpleSBML
 from SBMLLint.common import simple_sbml
-from SBMLLint.structured_names.moiety import Moiety
+from SBMLLint.structured_names.moiety import Moiety, MoietyComparator
 
+import itertools
 import numpy as np
 import os
 import tesbml
@@ -16,14 +17,28 @@ import unittest
 IGNORE_TEST = False
 MOIETY_NAME1 = "first"
 MOIETY_NAME2 = "second"
+MOIETY_NAME3 = "third"
+NAMES = [MOIETY_NAME1, MOIETY_NAME2, MOIETY_NAME3]
 MOLECULE_NAME = "%s%s%s" % (MOIETY_NAME1, cn.MOIETY_SEPARATOR, 
     MOIETY_NAME2)
+iterator = itertools.product([0,1], [0, 1], [0, 1])
+MOLECULE_NAME_SET = []
+for item in iterator:
+  name = ""
+  for idx,ele in enumerate(item):
+    if ele == 1:
+      if len(name) == 0:
+        name = NAMES[idx]
+      else:
+        name = "%s%s%s" % (name, cn.MOIETY_SEPARATOR, NAMES[idx])
+  if len(name) > 0:
+    MOLECULE_NAME_SET.append(name)
 
 
 #############################
 # Tests
 #############################
-class TestMolecule(unittest.TestCase):
+class TestMoiety(unittest.TestCase):
 
   def setUp(self):
     self.simple = SimpleSBML(cn.TEST_FILE)
@@ -55,6 +70,29 @@ class TestMolecule(unittest.TestCase):
     df = Moiety.countMoietys([molecule])
     df2 = Moiety.countMoietys([molecule, molecule])
     self.assertTrue(df2.equals(df + df))
+
+
+class TestMoietyComparator(unittest.TestCase):
+
+  def setUp(self):
+    self.molecules1 = [Molecule(n) for n in MOLECULE_NAME_SET[:3]]
+    self.molecules2 = [Molecule(n) for n in MOLECULE_NAME_SET[4:]]
+    self.comparator = MoietyComparator(self.molecules1,
+        self.molecules2)
+
+  def testConstructor(self):
+    self.assertEqual(len(self.comparator.list_of_molecules), 2)
+
+  def testIsSame(self):
+    self.assertFalse(self.comparator.isSame())
+    comparator = MoietyComparator(self.molecules1,
+        self.molecules1)
+    self.assertTrue(comparator.isSame())
+
+  def testDifference(self):
+    df = self.comparator.difference()
+    self.assertLess(df.loc[MOIETY_NAME1].tolist()[0], 0)
+    
     
 
 if __name__ == '__main__':
