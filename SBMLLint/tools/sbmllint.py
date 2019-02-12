12d0ddd9ddf2199105ed01e1640c5d7da9b8e0f2
Joe Hellerstein <jlheller@uw.edu>
2019-02-12 13:58:28 -0800

    Small updates

--- SBMLLint/tools/sbmllint.py
+++ SBMLLint/tools/sbmllint.py
@@ -11,22 +11,29 @@ import sys
 
 
 def lint(model_reference, file_out=sys.stdout,
-    mass_balance_check="structured_names"):
+    mass_balance_check="structured_names",
+    is_report=True):
   """
   Reports on errors found in a model
-  :param str model_reference: file, antimony string, xml string
+  :param str model_reference: 
+      libsbml_model, file, antimony string, xml string
   :param TextIOWrapper file_out:
   :param str mass_balance_check: how check for mass balance
+  :param bool is_report: print result
   :return int, int: total reactions, number non-compliant
   """
-  document = util.getSBMLDocument(model_reference)
-  model = document.getModel()
+  if "Model" in str(type(model_reference)):
+    model = model_reference
+  else:
+    document = util.getSBMLDocument(model_reference)
+    model = document.getModel()
   num_bad, report = MoietyComparator.analyzeReactions(model)
   num_reactions = len(Reaction.reactions)
-  file_out.write("%d/%d reactions are impbalanced." 
-      % (num_bad, num_reactions))
-  for line in report.split('\n'):
-    file_out.write("%s\n" % line)
+  if is_report:
+    file_out.write("%d/%d reactions are impbalanced." 
+        % (num_bad, num_reactions))
+    for line in report.split('\n'):
+        file_out.write("%s\n" % line)
   return num_reactions, num_bad
     
 
