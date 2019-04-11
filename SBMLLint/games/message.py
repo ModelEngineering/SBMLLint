2a6a70b6c3ce0d6e1cae962d8c8f1822aece1ce6
woosubs <woosubs@umich.edu>
2019-04-11 12:20:00 -0700

    started SOMStoichiometryMatrix

--- SBMLLint/games/message.py
+++ SBMLLint/games/message.py
@@ -3,6 +3,7 @@
 from SBMLLint.common import constants as cn
 from SBMLLint.common.molecule import MoleculeStoichiometry
 from SBMLLint.common.reaction import Reaction
+from SBMLLint.common import util
 from SBMLLint.games.som import SOM
 from SBMLLint.common.simple_sbml import SimpleSBML
 
@@ -12,11 +13,24 @@ import networkx as nx
 import numpy as np
 import pandas as pd
 #import scipy
-from sympy.matrices import Matrix, eye
+from scipy.linalg import lu, inv
 
 # BIOMD 383 will test the validity of Gaussian Elimination to find errors, 
-# before proceeding to building MESGraph
+# before proceeding to building SOMGraph
 
+
+class SOMStoichiometry(object):
+
+  def __init__(self, som, stoichiometry):
+    if not isinstance(som, SOM):
+      raise ValueError("First argument must be a SOM.")
+    if not util.isFloat(stoichiometry):
+      raise ValueError("Second argument must be a float.")
+    self.som = som
+    self.stoichiometry = stoichiometry
+
+  def __repr__(self):
+    return "%s * % 2.2f" % (str(self.som), self.stoichiometry)
 
 
 class Message(nx.DiGraph):
@@ -39,16 +53,21 @@ class Message(nx.DiGraph):
     self.molecules = self._getNonBoundaryMolecules(simple, self.reactions)
     self.stoichiometry_matrix = self.getStoichiometryMatrix(self.reactions, self.molecules)
     self.reduced_reactions = []
-    # INVERSE of L from LU decomposition
-    self.lower_inverse = None
+    self.multimulti_reactions = []
     self.permuted_matrix = None
+    # L matrix from LU decomposition (not always invertible)
+    self.lower_inverse  = None
+    self.rref_df = None
     # Components for SOMGraph
     super(Message, self).__init__()
     self.soms = self.initializeSOMs(self.molecules)
     # networkx method
     self.add_nodes_from(self.soms)
     self.identifier = self.makeId()
-
+    # storing errors
+    self.rref_errors = []
+    self.type_one_errors = []
+    self.type_two_errors = []
   #
   def __repr__(self):
     return self.identifier
@@ -139,20 +158,23 @@ class Message(nx.DiGraph):
     mat_t = mat_df.T
     idx_mat_t = mat_t.index
     # LU decomposition
-    m = Matrix(mat_df.T)
-    lower, upper, perm = m.LUdecomposition()
-    permuted_m = m.permuteFwd(perm)
-    new_idx_mat_t = list(Matrix(idx_mat_t).permuteFwd(perm))
-    perm_df = pd.DataFrame(np.array(permuted_m).astype(np.float64),
+    perm, lower, upper = lu(mat_t)
+    # the following is applicable to transposed stoichiometry matrix
+    permuted_m = inv(perm).dot(mat_t)
+    pivot_index = [list(k).index(1) for k in inv(perm)]
+    new_idx_mat_t = [idx_mat_t[idx] for idx in pivot_index]
+    # we save as; perm_df * lower_operation * mat_t = rref_df
+    perm_df = pd.DataFrame(permuted_m,
         index=new_idx_mat_t,
         columns=mat_t.columns).T
-    rref_df = pd.DataFrame(np.array(upper).astype(np.float64),
+    rref_df = pd.DataFrame(upper,
         index=new_idx_mat_t,
         columns=mat_t.columns).T
-    lower_operation = pd.DataFrame(np.array(lower.inv()).astype(np.float64),
+    lower_operation = pd.DataFrame(lower,
         columns=new_idx_mat_t)
     self.permuted_matrix = perm_df
     self.lower_inverse = lower_operation
+    self.rref_df = rref_df
     return rref_df
   #
   def getReactionSummaryCategory(self, reactants, products):
@@ -207,17 +229,330 @@ class Message(nx.DiGraph):
                                       category=self.getReactionSummaryCategory(reactants, products)))
     return reactions
   #
-  def analyze(self, error_details=True):
+  def getNode(self, molecule):
+    """
+    Find a node(SOM) containing the given molecule.
+    If no such SOM exists, return False
+    :param Molecule molecule:
+    :return SOM/False:
+    """
+    for som in list(self.nodes):
+      for mole in som.molecules:
+        if mole.name == molecule.name:
+          return som
+    return False
+  #
+  def mergeNodes(self, som1, som2, reaction):
+    """
+    Merge two nodes (SOMs).
+    Update arcs if applicable. 
+    :param SOM som1:
+    :param SOM som2:
+    :param Reaction reaction:
+    :return SOM new_som:
+    """
+    new_som = som1.merge(som2)
+    new_som.reactions.add(self.simple.getReaction(reaction.label))
+    for som in [som1, som2]:
+      for edge in list(self.in_edges(som)):
+        remaining_som = edge[0]
+        reaction_label = self.get_edge_data(edge[0], edge[1])[cn.REACTION]
+        self.add_edge(remaining_som, new_som, reaction=reaction_label)  
+      for edge in list(self.out_edges(som)):
+        remaining_som = edge[1]
+        reaction_label = self.get_edge_data(edge[0], edge[1])[cn.REACTION]
+        self.add_edge(new_som, remaining_som, reaction=reaction_label) 
+    self.remove_nodes_from([som1, som2])
+    if not self.has_node(new_som):
+      self.add_node(new_som)
+    return new_som
+  #
+  def addMultiMultiReaction(self, reaction=None):
+    """
+    Add a multi-multi reaction to self.multimulti_reactions
+    :param reaction Reaction:
+    :return bool:
+    """
+    if reaction not in self.multimulti_reactions:
+      self.multimulti_reactions.append(reaction)  
+  #
+  def processUniUniReaction(self, reaction):
+    """
+    Process a 1-1 reaction to merge nodes.
+    If no need to merge, return None.
+    :param Reaction reaction:
+    """
+    if reaction.category != cn.REACTION_1_1:
+      pass
+    else:
+      reactant_som = self.getNode(reaction.reactants[0].molecule)
+      product_som = self.getNode(reaction.products[0].molecule)
+      if reactant_som == product_som:
+        return None
+      else:
+        new_som = self.mergeNodes(reactant_som, product_som, reaction)
+        self.identifier = self.makeId()
+        return new_som
+  #
+  def addArc(self, arc_source, arc_destination, reaction):
+    """
+    Add a single arc (edge) using two SOMs and reaction.
+    :param SOM arc_source:
+    :param SOM arc_destination:
+    :param Reaction reaction:
+    """
+    # if there is already a preious reaction,
+    if self.has_edge(arc_source, arc_destination):
+      reaction_label = self.get_edge_data(arc_source, arc_destination)[cn.REACTION]
+      # if reaction.label is not already included in the attribute,
+      if reaction.label not in set(reaction_label):
+        reaction_label = reaction_label + [reaction.label]
+    else:
+      reaction_label = [reaction.label]
+    # overwrite the edge with new reactions set
+    self.add_edge(arc_source, arc_destination, reaction=reaction_label)
+  # 
+  def processUniMultiReaction(self, reaction):
+    """
+    Process a 1-n reaction to add arcs.
+    Since the mass of reactant is greater than
+    that of each product, it adds arcs by
+    addArc(source=products, destination=reactant).
+    :param Reaction reaction:
+    """
+    if reaction.category != cn.REACTION_1_n:
+      pass
+    else:
+      destination = [reaction.reactants[0].molecule]
+      source = [product.molecule for product in reaction.products]
+      arcs = itertools.product(source, destination)
+      for arc in arcs:
+        if not self.checkTypeOneError(arc, reaction):
+          som_source = self.getNode(arc[0])
+          som_destination = self.getNode(arc[1])
+          self.addArc(som_source, som_destination, reaction)
+      self.identifier = self.makeId()
+  #
+  def processMultiUniReaction(self, reaction):
+    """
+    Process a n-1 reaction to add arcs.
+    Since the mass of product is greater than
+    that of each reactant, it adds arcs by
+    addArc(source=reactants, destination=product).
+    :param Reaction reaction:
+    """
+    if reaction.category != cn.REACTION_n_1:
+      pass
+    else:
+      destination = [reaction.products[0].molecule]
+      source = [reactant.molecule for reactant in reaction.reactants]
+      arcs = itertools.product(source, destination)
+      for arc in arcs:
+        if not self.checkTypeOneError(arc, reaction):
+          som_source = self.getNode(arc[0])
+          som_destination = self.getNode(arc[1])
+          self.addArc(som_source, som_destination, reaction)
+      self.identifier = self.makeId()
+  #
+  def addTypeOneError(self, mole1, mole2, reaction):
+    """
+    Add Type I Error components to self.type_one_errors
+    All components of resulting PathComponents are str
+    :param Molecule mole1:
+    :param Molecule mole2:
+    :param Reaction reaction:
+    :return bool flag:
+    """
+    flag = False
+    for component in self.type_one_errors:
+      if (component.node1==mole1.name) and (component.node2==mole2.name):
+        new_component = cn.PathComponents(node1=mole1.name, 
+                                          node2=mole2.name,
+                                          reactions=component.reactions+[reaction.label])
+        self.type_one_errors.remove(component)
+        self.type_one_errors.append(new_component)
+        flag = True
+        break
+    if not flag:
+      self.type_one_errors.append(cn.PathComponents(node1=mole1.name, 
+                                                    node2=mole2.name,
+                                                    reactions=[reaction.label]))
+      flag = True
+    return flag
+  #
+  def checkTypeOneError(self, arc, inequality_reaction=None):
+    """
+    Check Type I Error of an arc.
+    If both source and destination are found
+    in the same SOM, send error message and return True.
+    If not, return False.
+    :param tuple-Molecule arc:
+    :param Reaction inequality_reaction:
+    :return bool:
+    """
+    som1 = self.getNode(arc[0])
+    som2 = self.getNode(arc[1])
+    if som1 == som2:
+      self.addTypeOneError(arc[0], arc[1], inequality_reaction)
+      return True
+    else:
+      return False
+  #
+  def addTypeTwoError(self, cycle):
+    """
+    Add Type II Error components to self.type_two_errors
+    which is a list of lists
+    All components of resulting PathComponents are str
+    :param list-SOM cycle:
+    """
+    # exceptionally, here PathComponents are
+    # node1=[], node2=[], reactions=[] and their index
+    # of each component will match. All elements within nodes
+    # are in the same SOM
+    error_cycle = []
+    for node_idx in range(len(cycle)-1):
+      som1 = cycle[node_idx]
+      som2 = cycle[node_idx+1]
+      som1_moles = {mole.name for mole in list(som1.molecules)}
+      som2_moles = {mole.name for mole in list(som2.molecules)}
+      reactions = self.get_edge_data(som1, som2)[cn.REACTION]
+      # all reactions (in an edge), should create a single PathComponent
+      nodes1 = []
+      nodes2 = []
+      reaction_labels = []
+      for r in reactions:
+        reaction = self.simple.getReaction(r)
+        if reaction.category == cn.REACTION_n_1:
+          sources = {r.molecule.name for r in reaction.reactants}
+          destinations = {p.molecule.name for p in reaction.products}
+        elif reaction.category == cn.REACTION_1_n:
+          sources = {p.molecule.name for p in reaction.products}
+          destinations = {r.molecule.name for r in reaction.reactants}
+        # for any reaction that addes arcs, len(nodes2)==1
+        node2 = list(destinations.intersection(som2_moles))[0]
+        for node1 in list(sources.intersection(som1_moles)):
+          nodes1.append(node1)
+          nodes2.append(node2)
+          reaction_labels.append(reaction.label)
+      error_cycle.append(cn.PathComponents(node1=nodes1, 
+                                           node2=nodes2,
+                                           reactions=reaction_labels))
+    som1 = cycle[-1]
+    som2 = cycle[0]
+    som1_moles = {mole.name for mole in list(som1.molecules)}
+    som2_moles = {mole.name for mole in list(som2.molecules)}
+    reactions = self.get_edge_data(som1, som2)[cn.REACTION]
+    # all reactions (in an edge), should create a single PathComponent
+    nodes1 = []
+    nodes2 = []
+    reaction_labels = []
+    for r in reactions:
+      reaction = self.simple.getReaction(r)
+      if reaction.category == cn.REACTION_n_1:
+        sources = {r.molecule.name for r in reaction.reactants}
+        destinations = {p.molecule.name for p in reaction.products}
+      elif reaction.category == cn.REACTION_1_n:
+        sources = {p.molecule.name for p in reaction.products}
+        destinations = {r.molecule.name for r in reaction.reactants}
+        # for any reaction that addes arcs, len(nodes2)==1
+      node2 = list(destinations.intersection(som2_moles))[0]
+      for node1 in list(sources.intersection(som1_moles)):
+        nodes1.append(node1)
+        nodes2.append(node2)
+        reaction_labels.append(reaction.label)
+    error_cycle.append(cn.PathComponents(node1=nodes1, 
+                                        node2=nodes2,
+                                        reactions=reaction_labels))
+    self.type_two_errors.append(error_cycle)  
+  #
+  def checkTypeTwoError(self):
+    """
+    Check Type II Error (cycles) of a MESGraph.
+    If there is at least one cycle, 
+    report an error message, related reactions
+    and return True.
+    If there is no cycle, return False. 
+    :return bool:
+    """
+    graph = nx.DiGraph()
+    graph.add_edges_from(self.edges)
+    cycles = list(nx.simple_cycles(graph))
+    if len(cycles) == 0:
+      return False
+    else:
+      for cycle in cycles:
+        ######self.addTypeTwoError(cycle)######
+        self.type_two_errors.append(cycle)
+        # if not self.type_two_error:
+        #   self.type_two_error = True
+      return True
+  #
+  def processErrorReactions(self, reaction):
+    """
+    Simply add error reactions to error
+    :param Reaction reaction:
+    :return bool:
+    """
+    error_reactions = [r.label for r in self.rref_errors]
+    if reaction.label not in error_reactions:
+      self.rref_errors.append(reaction)
+    ## Here, we may need to add more info that will 
+    ## help us track the operations that lead to this error 
+    return True
+  #
+  def analyze(self, reactions=None, error_details=True):
     """
     Using the stoichiometry matrix, compute
     row reduced echelon form and create SOMGraph
     Add arcs or sending error messages using
     checkTypeOneError or checkTypeTwoError.
     :param bool error_details:
-    :return str:
+    :return bool:
     """
-    report = NULL_STR
-    pass
+    if reactions is None:
+      reactions = self.simple.reactions
+    # Associate the reaction category with the function
+    # that processes that category
+    report = cn.NULL_STR
+    reaction_dic = {
+        cn.REACTION_1_1: self.processUniUniReaction,
+        cn.REACTION_1_n: self.processUniMultiReaction,
+        cn.REACTION_n_1: self.processMultiUniReaction,
+        cn.REACTION_n_n: self.addMultiMultiReaction,
+        }
+    # Process each type of reaction
+    for category in reaction_dic.keys():
+      for reaction in [r for r in reactions if r.category == category]:
+        func = reaction_dic[category]
+        func(reaction)
+    #
+
+    # Decompose matrix and prepare reduced reactions
+    rref_df = self.decomposeMatrix(self.stoichiometry_matrix)
+    reduced_reactions = self.convertMatrixToReactions(self.simple, rref_df)
+    self.reduced_reactions = reduced_reactions
+    report = cn.NULL_STR
+    reaction_dic = {
+        cn.REACTION_ERROR: self.processErrorReactions,
+        cn.REACTION_1_1: self.processUniUniReaction,
+        cn.REACTION_1_n: self.processUniMultiReaction,
+        cn.REACTION_n_1: self.processMultiUniReaction
+        }
+    # Process each type of reaction
+    for category in reaction_dic.keys():
+      for reaction in [r for r in reduced_reactions if r.category == category]:
+        func = reaction_dic[category]
+        func(reaction)
+    #
+    self.checkTypeTwoError()
+    # print("We just analyzed the data...")
+    # print("RREF error: ", self.rref_errors)
+    # print("Type I error: ", self.type_one_errors)
+    # print("Type II error: " , self.type_two_errors)
+    if self.rref_errors or self.type_one_errors or self.type_two_errors:
+      return True
+    else:
+      return False
 
 
 
