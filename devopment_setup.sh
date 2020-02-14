cca97f31e53f80779c07c437b95a79abe7891f4c
Joseph Hellerstein <jlheller@uw.edu>
2020-02-13 19:03:47 -0800

    Travis diagnostics

--- devopment_setup.sh
+++ devopment_setup.sh
@@ -4,3 +4,4 @@ conda env create -f environment.yml
 conda activate sbmllint
 pip install python-libsbml
 pip install tellurium
+sudo apt-get install python3-venv
--- run_install.sh
+++ run_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3 -m venv test-sbmllint
 source test-sbmllint/bin/activate
-python setup.py install
+python3 setup.py install
 echo "Success."
 echo "Do: test-sbmllint/bin/activate before using installed codes."
