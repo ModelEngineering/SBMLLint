6998ed8e582ebfbb01b9f82014e08afeacbe49b2
Joseph Hellerstein <jlheller@uw.edu>
2020-02-16 19:50:27 -0800

    games and moiety_analysis can process zip files.

--- SBMLLint/tools/games.py
+++ SBMLLint/tools/games.py
@@ -4,6 +4,7 @@ Runs the GAMES algorithm for a local XML file.
 Usage: games <filepath>
 """
 
+from SBMLLint.common import util
 from SBMLLint.tools import sbmllint
 
 import argparse
@@ -14,8 +15,12 @@ def main():
   parser.add_argument('--config', type=open,
       help="SBMLLint configuration file")
   args = parser.parse_args()
-  sbmllint.lint(model_reference=args.xml_file,
-      config_fid=args.config, mass_balance_check=sbmllint.GAMES)
+  for fid in util.getNextFid(args.xml_file):
+    try:
+      sbmllint.lint(model_reference=fid,
+          config_fid=args.config, mass_balance_check=sbmllint.GAMES)
+    except ValueError:
+      print ("  *** Bad SBML file.")
 
 
 if __name__ == '__main__':
--- SBMLLint/tools/moiety_analysis.py
+++ SBMLLint/tools/moiety_analysis.py
@@ -5,19 +5,24 @@ Usage: moiety_analysis <filepath>
 """
 
 from SBMLLint.common import constants as cn
+from SBMLLint.common import util
 from SBMLLint.tools import sbmllint
 
 import argparse
 
 def main():
   parser = argparse.ArgumentParser(description='SBML XML file.')
-  parser.add_argument('xml_file', type=open, help='SBML file')
+  parser.add_argument('xml_file', type=open, help='SBML or zip file')
   parser.add_argument('--config', type=open,
       help="SBMLLint configuration file")
   args = parser.parse_args()
-  sbmllint.lint(model_reference=args.xml_file,
-      mass_balance_check=cn.MOIETY_ANALYSIS,
-      config_fid=args.config)
+  for fid in util.getNextFid(args.xml_file):
+    try:
+      sbmllint.lint(model_reference=fid,
+          mass_balance_check=cn.MOIETY_ANALYSIS,
+          config_fid=args.config)
+    except ValueError:
+      print ("  *** Bad SBML file.")
 
 
 if __name__ == '__main__':
