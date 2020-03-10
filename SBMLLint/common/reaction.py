1b8d4e006c1d643866684d352cf1c110ce3c9232
Joseph Hellerstein <jlheller@uw.edu>
2020-03-10 12:08:50 -0700

    Eliminate incorrect use of SpeciesReference where use species property instead of getSpecies()

--- SBMLLint/common/reaction.py
+++ SBMLLint/common/reaction.py
@@ -19,9 +19,9 @@ def getMolecules(libsbml_reaction, func):
   species = func(libsbml_reaction)
   molecule_stoichiometrys = []
   for spc in species:
-    molecule = Molecule.getMolecule(spc.species)
+    molecule = Molecule.getMolecule(spc.getSpecies())
     if molecule is None:
-        molecule = Molecule(spc.species)
+        molecule = Molecule(spc.getSpecies())
     molecule_stoichiometrys.append(MoleculeStoichiometry(
         molecule,
         spc.getStoichiometry())
@@ -58,7 +58,7 @@ class Reaction(object):
     result = []
     collection = [func_get_one(n) for n in range(func_get_num())]
     for s_r in collection:
-      molecule = Molecule(s_r.species)
+      molecule = Molecule(s_r.getSpecies())
       stoich = s_r.getStoichiometry()
       result.append(MoleculeStoichiometry(molecule, stoich))
     return result
