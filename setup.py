ef0cbb7d550ada1dafab4218b9a5d75d7ec5c29e
Joseph Hellerstein <jlheller@uw.edu>
2020-04-07 08:53:18 -0700

    Updated pip version number.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ INSTALL_REQUIRES = [
 def doSetup(install_requires):
   setup(
       name='SBMLLint',
-      version='1.1.0',
+      version='1.1.1',
       author='Woosub Shin, Joseph L. Hellerstein',
       author_email='jlheller@uw.edu',
       packages=find_packages(exclude=['tests', 'analysis',
